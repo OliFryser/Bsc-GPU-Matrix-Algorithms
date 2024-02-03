71f7731bfe096845d34f05800cb963dcb36ba670
William Skou Heidemann <williamskou2227@gmail.com>
2024-02-03 15:57:36 +0100

    Add plotting of data and a directory called Diagrams

--- AutomationAndBenchmarking/Benchmark.py
+++ AutomationAndBenchmarking/Benchmark.py
@@ -1,14 +1,19 @@
 import subprocess
 import os
+import csv
+import matplotlib.pyplot as pyplot
 
 directory = "MatrixAlgorithms/"
 source_files = [os.path.join(directory, name) for name in os.listdir(directory) if name.endswith(".c")]
 binary_path = directory + "binary"
 compile_command = ["gcc", "-o", binary_path] + source_files
+data_save_file = "DummyData/RandomData.csv"
+number_count = "5"
 
+# Run C-program to generate data
 try:
     subprocess.run(compile_command, check=True)
-    subprocess.run([binary_path], check=True)
+    subprocess.run([binary_path, data_save_file, number_count], check=True)
     os.remove(binary_path)
 except FileNotFoundError as e:
     print(f"File not found error: {e}")
@@ -19,3 +24,16 @@ except subprocess.CalledProcessError as e:
 except Exception as e:
     print(f"An unexpected error occurred: {e}")
     exit()
+
+# Visualize data with matplotlib
+with open(data_save_file, 'r') as file:
+    csv_reader = csv.reader(file)
+    for row in csv_reader:
+        numbers = list(map(int, row))
+        x = [0, 1, 2, 3, 4]
+        pyplot.plot(x, numbers)
+
+pyplot.xticks([0, 1, 2, 3, 4])
+pyplot.ylabel("Number Generated")
+pyplot.title("Random Numbers")
+pyplot.savefig("Diagrams/output_plot.png")
--- MatrixAlgorithms/Program.c
+++ MatrixAlgorithms/Program.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "ArrayAlgorithms.h"
 #include "DataWriter.h"
 
@@ -6,13 +7,22 @@ int *random_numbers(int size, int max_value);
 FILE *createCSV(char *csvPath);
 void writeToCSV(FILE *file, int data[], int size_of_array);
 
-int main() {
-    int *numbers;
+int main(int argc, char *argv[]) {
+    char *save_file_name;
     FILE *file;
-    const int number_count = 5;
+    int number_count;
+    int *numbers;
+
+    if (argc < 3) {
+        printf("Usage: %s <file_path.csv> <number_count>\n", argv[0]);
+        return 0;
+    }
+
+    save_file_name = argv[1];
+    number_count = atoi(argv[2]);
 
     numbers = random_numbers(number_count, 10);
-    file = createCSV("DummyData/RandomData.csv");
+    file = createCSV(save_file_name);
     if (file == NULL) return 0;
     writeToCSV(file, numbers, number_count);
 
