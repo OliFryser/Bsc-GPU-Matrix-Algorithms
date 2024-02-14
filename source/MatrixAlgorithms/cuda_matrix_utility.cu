4dd82299d11e9e5d5f8483b2e3fd8aed4f31528f
William Skou Heidemann <williamskou2227@gmail.com>
2024-02-14 14:20:08 +0100

    extern c

--- source/MatrixAlgorithms/cuda_matrix_utility.cu
+++ source/MatrixAlgorithms/cuda_matrix_utility.cu
@@ -1,16 +1,18 @@
+extern "C" {
 #include "cuda_matrix_utility.h"
+}
 
-Matrix *cuda_matrix_init(int rows, int columns) {
+extern "C" Matrix *cuda_matrix_init(int rows, int columns) {
     return NULL;
 }
 
-void cuda_matrix_free(Matrix *matrix) {
+extern "C" void cuda_matrix_free(Matrix *matrix) {
 
 }
 
-void cuda_matrix_host_to_device(Matrix *dst, Matrix* src) {
+extern "C" void cuda_matrix_host_to_device(Matrix *dst, Matrix* src) {
 
 }
-void cuda_matrix_device_to_host(Matrix *dst, Matrix* src) {
+extern "C" void cuda_matrix_device_to_host(Matrix *dst, Matrix* src) {
     
 }
