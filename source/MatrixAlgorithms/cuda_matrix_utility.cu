1d249c188e4c99aa2134d705f8b7976208931e3c
William Skou Heidemann <williamskou2227@gmail.com>
2024-02-14 12:14:36 +0100

    methods

---
+++ source/MatrixAlgorithms/cuda_matrix_utility.cu
@@ -0,0 +1,11 @@
+#include "cuda_matrix_algorithms.h"
+
+Matrix *cuda_matrix_init(int rows, int columns) {
+    return NULL;
+}
+void cuda_matrix_host_to_device(Matrix *dst, Matrix* src) {
+
+}
+void cuda_matrix_device_to_host(Matrix *dst, Matrix* src) {
+    
+}
