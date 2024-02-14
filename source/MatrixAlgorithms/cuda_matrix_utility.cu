2cbf39d62a46a5c5faa3fc28724d4bdc93bb5f01
oljh <oljh@itu.dk>
2024-02-14 15:23:58 +0100

    add cuda_matrix_utility

--- source/MatrixAlgorithms/cuda_matrix_utility.cu
+++ source/MatrixAlgorithms/cuda_matrix_utility.cu
@@ -3,16 +3,32 @@ extern "C" {
 }
 
 extern "C" Matrix *cuda_matrix_init(int rows, int columns) {
-    return NULL;
+    Matrix *device_matrix;
+    cudaMalloc(&device_matrix, sizeof(Matrix));
+    if (device_matrix != NULL) return NULL;
+    device_matrix->rows = rows;
+    device_matrix->columns = columns;
+
+    cudaMalloc(&device_matrix->values, rows * sizeof(float *));
+
+    for (int i = 0; i < rows; i++) {
+        cudaMalloc(&device_matrix->values[i], sizeof(columns * sizeof(float)));
+    }
+
+    return device_matrix;
 }
 
-extern "C" void cuda_matrix_free(Matrix *matrix) {
-
+extern "C" void cuda_matrix_free(Matrix *device_matrix) {
+    if (device_matrix == NULL) return;
+    if (device_matrix->values != NULL) {
+        for (int i = 0; i < device_matrix->rows; i++) {
+            if (device_matrix->values[i] != NULL)
+                cudaFree(device_matrix->values[i]);
+        }
+        cudaFree(device_matrix->values);
+    }
+    cudaFree(device_matrix);
 }
 
-extern "C" void cuda_matrix_host_to_device(Matrix *dst, Matrix* src) {
-
-}
-extern "C" void cuda_matrix_device_to_host(Matrix *dst, Matrix* src) {
-    
-}
+extern "C" void cuda_matrix_host_to_device(Matrix *dst, Matrix *src) {}
+extern "C" void cuda_matrix_device_to_host(Matrix *dst, Matrix *src) {}
