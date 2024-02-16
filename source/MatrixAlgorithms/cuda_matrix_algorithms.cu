99deaed8d41b46c9157ccc844a306cfd10f98605
William Skou Heidemann <williamskou2227@gmail.com>
2024-02-16 12:09:07 +0100

    matrix_addition_gpu_single_core passes test

--- source/MatrixAlgorithms/cuda_matrix_algorithms.cu
+++ source/MatrixAlgorithms/cuda_matrix_algorithms.cu
@@ -2,15 +2,23 @@ extern "C" {
 #include "cuda_matrix_algorithms.h"
 }
 
-__global__ void matrix_addition_gpu_single_core_kernel(DEVICE_MATRIX matrix1,
-                                                       DEVICE_MATRIX matrix2,
-                                                       DEVICE_MATRIX result,
-                                                       int size) {}
+__global__ void matrix_addition_gpu_single_core_kernel(
+    DEVICE_MATRIX matrix1,
+    DEVICE_MATRIX matrix2,
+    DEVICE_MATRIX result,
+    int size) {
+    
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = matrix1[i] + matrix2[i];
+    }
+}
 
 extern "C" void matrix_addition_gpu_single_core(DEVICE_MATRIX matrix1,
                                                 DEVICE_MATRIX matrix2,
                                                 DEVICE_MATRIX result,
                                                 int rows,
                                                 int columns) {
+    matrix_addition_gpu_single_core_kernel<<<1,1>>>(matrix1, matrix2, result, rows * columns);
     cudaDeviceSynchronize();
 }
