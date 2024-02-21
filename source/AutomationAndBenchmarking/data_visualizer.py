8b3874adf81142048787f0b557c0c63eeecf2f6f
William Skou Heidemann <williamskou2227@gmail.com>
2024-02-21 12:10:34 +0100

    new and improved data visualizer and data structure

--- source/AutomationAndBenchmarking/data_visualizer.py
+++ source/AutomationAndBenchmarking/data_visualizer.py
@@ -1,17 +1,24 @@
 import matplotlib.pyplot as pyplot
 from csv_data_structure import CSVDataStructure
 
-def visualize_csv(csv_data: CSVDataStructure, diagram_save_path):
-    input_sizes = csv_data.get_input_sizes()
-    algorithms = csv_data.get_algorithms()
-    for algorithm in algorithms:
-        run_times = sorted(csv_data.get_run_times(algorithm))
-        print(run_times)
-        pyplot.plot(input_sizes, run_times, label=algorithm)
+def visualize_csv(csv_data: CSVDataStructure, diagram_save_path: str, algorithms_to_compare: list[str]):
+    dimensions = csv_data.dimensions
+    
+    for algorithm in algorithms_to_compare:
+        if algorithm not in csv_data.data.keys(): continue
+        data = csv_data.data[algorithm]
+        run_time_means: list[float] = []
+        standard_deviations: list[float] = []
+        for dimension in dimensions:
+            run_time_means.append(data.dimension_to_mean_run_time[dimension])
+            standard_deviations.append(data.dimension_to_standard_deviation[dimension])
+
+        pyplot.plot(dimensions, run_time_means, label=algorithm)
     
     pyplot.title('Run time comparison')
-    pyplot.xlabel('Input size')
-    pyplot.ylabel('Run time')
-    pyplot.xscale('log')
+    pyplot.xlabel('Dimension')
+    pyplot.ylabel('Mean Run time')
+    pyplot.xscale('log', base=2)
+    pyplot.yscale('log', base=2)
     pyplot.legend()
     pyplot.savefig(diagram_save_path)
