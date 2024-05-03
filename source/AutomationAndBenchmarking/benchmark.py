36936dcdf912aa07facbb3bbf69caf6274675ed4
William Skou Heidemann <williamskou2227@gmail.com>
2024-05-03 10:55:30 +0200

    code debugging

--- source/AutomationAndBenchmarking/benchmark.py
+++ source/AutomationAndBenchmarking/benchmark.py
@@ -43,10 +43,10 @@ compile_command_2d = ["gcc", "-L/usr/local/cuda/lib64", "-o", binary_path_2d] + 
 
 timestamp = datetime.now().strftime("%m-%d %H:%M:%S")
 csv_path = "BenchmarkData/" + timestamp + ".csv"
-algorithms_to_run = [] # ["diagnostic: launch kernel 1 block 1 thread", "diagnostic: launch kernel scaling grid and blocks", "diagnostic: cudaMalloc", "diagnostic: cudaMemcpy", "diagnostic: cudaMemcpy & launch kernel 1 block 1 thread", "diagnostic: cudaMemcpy & launch larger kernel"]
-additional_algorithms_to_compare = [qr_gpu_single_core, qr_gpu_parallel_max]
-additional_csv_files_to_include = ["BenchmarkData/05-03 07:40:54.csv"]
-matrix_dimensions = [math.floor(2 ** (i)) for i in range(0, 11)] #, 1_000, 10_000, 100_000, 1_000_000]
+algorithms_to_run = [qr_gpu_parallel_max] # ["diagnostic: launch kernel 1 block 1 thread", "diagnostic: launch kernel scaling grid and blocks", "diagnostic: cudaMalloc", "diagnostic: cudaMemcpy", "diagnostic: cudaMemcpy & launch kernel 1 block 1 thread", "diagnostic: cudaMemcpy & launch larger kernel"]
+additional_algorithms_to_compare = [] #[qr_gpu_single_core, qr_gpu_parallel_max]
+additional_csv_files_to_include = [] #["BenchmarkData/05-03 07:40:54.csv"]
+matrix_dimensions = [math.floor(2 ** (i)) for i in range(9, 10)] #, 1_000, 10_000, 100_000, 1_000_000]
 diagram_save_path = "Diagrams/output_plot" + timestamp + ".png"
 
 try:
--- source/Tests/test_runner.c
+++ source/Tests/test_runner.c
@@ -47,95 +47,96 @@ int main() {
 
     /* add the tests to the suite */
     /* NOTE - ORDER IS IMPORTANT - MUST TEST fread() AFTER fprintf() */
-    if ((NULL == CU_add_test(
-                     matrix_suite, "matrix_t Init Test", test_init_matrix)) ||
-        (NULL == CU_add_test(matrix_suite, "matrix_t Init 2x2 From CSV Test",
-                     test_init_matrix_2x2_from_csv)) ||
-        (NULL == CU_add_test(matrix_suite, "matrix_t Init 4x1 From CSV Test",
-                     test_init_matrix_4x1_from_csv)) ||
-        (NULL == CU_add_test(matrix_suite,
-                     "matrix_t Init 2x2 doubled From CSV Test",
-                     test_init_matrix_2x2_doubled_from_csv)) ||
-        (NULL == CU_add_test(matrix_suite, "matrix_t dimension equality",
-                     test_matrix_equal_dimensions)) ||
-        (NULL == CU_add_test(matrix_suite, "matrix_t dimension inequality",
-                     test_matrix_not_equal_dimensions)) ||
-        (NULL == CU_add_test(
-                     matrix_suite, "matrix_t equality", test_matrix_equal)) ||
-        (NULL == CU_add_test(matrix_suite, "almost equal not",
-                     test_almost_equal_not)) ||
-        (NULL ==
-            CU_add_test(matrix_suite, "almost equal", test_almost_equal)) ||
-        (NULL == CU_add_test(matrix_suite, "matrix_t almost equal",
-                     test_matrix_almost_equal)) ||
-        (NULL ==
-            CU_add_test(matrix_suite, "matrix_t copy", test_matrix_copy)) ||
-        (NULL == CU_add_test(matrix_suite, "matrix_t addition",
-                     test_matrix_addition)) ||
-        (NULL == CU_add_test(matrix_suite, "matrix_t multiplication cpu",
-                     test_matrix_multiplication)) ||
-        (NULL == CU_add_test(matrix_suite, "matrix_t qr decomposition cpu",
-                     test_matrix_qr_decomposition)) ||
-        (NULL == CU_add_test(matrix_suite, "matrix_t qr 2 decomposition cpu",
-                     test_matrix_qr_2_decomposition)) ||
-        (NULL == CU_add_test(cuda_matrix_suite,
-                     "matrix_t addition gpu single core",
-                     test_matrix_addition_gpu_single_core)) ||
-        (NULL == CU_add_test(matrix_suite, "matrix_t Init Test Bad Values",
-                     test_init_matrix_0_values)) ||
-        (NULL == CU_add_test(matrix_suite, "matrix_t Random Fill Test In Range",
-                     test_matrix_random_fill)) ||
-        (NULL == CU_add_test(cuda_matrix_suite, "Test cuda matrix utility",
-                     test_cuda_matrix_utility)) ||
-        (NULL == CU_add_test(cuda_matrix_suite,
-                     "matrix_t addition gpu multi core",
-                     test_matrix_addition_gpu_multi_core)) ||
-        (NULL == CU_add_test(cuda_matrix_suite,
-                     "matrix_t addition gpu multi core 2",
-                     test_matrix_addition_gpu_multi_core2)) ||
-        (NULL == CU_add_test(cuda_matrix_suite,
-                     "matrix_t addition gpu multi core 2 larger matrices",
-                     test_matrix_addition_gpu_multi_core2_larger_matrices)) ||
-        (NULL == CU_add_test(cuda_matrix_suite, "matrix_t addition gpu blocks",
-                     test_matrix_addition_gpu_blocks)) ||
-        (NULL == CU_add_test(cuda_matrix_suite,
-                     "matrix_t multiplication gpu single core",
-                     test_matrix_multiplication_gpu_single_core)) ||
-        (NULL == CU_add_test(cuda_matrix_suite,
-                     "matrix_t multiplication gpu multi core: unwrapping i",
-                     test_matrix_multiplication_gpu_multi_core_unwrapping_i)) ||
-        (NULL ==
-            CU_add_test(cuda_matrix_suite,
-                "matrix_t multiplication gpu multi core: unwrapping i and j",
-                test_matrix_multiplication_gpu_multi_core_unwrapping_i_and_j)) ||
-        (NULL ==
-            CU_add_test(cuda_matrix_suite,
-                "matrix_t multiplication gpu multi core: unwrapping i and j "
-                "for "
-                "larger matrices",
-                test_matrix_multiplication_gpu_multi_core_unwrapping_i_and_j_larger_matrices)) ||
-        (NULL ==
-            CU_add_test(cuda_matrix_suite,
-                "matrix_t multiplication gpu multi core: shared memory",
-                test_matrix_multiplication_gpu_multi_core_shared_memory)) ||
-        (NULL ==
-            CU_add_test(cuda_matrix_suite,
-                "matrix_t multiplication gpu multi core: shared memory for "
-                "larger matrices",
-                test_matrix_multiplication_gpu_multi_core_shared_memory_larger_matrices)) ||
-        (NULL ==
-            CU_add_test(cuda_matrix_suite,
-                "matrix_t multiplication gpu multi core: shared memory fewer "
-                "accesses",
-                test_matrix_multiplication_gpu_multi_core_shared_memory_fewer_accesses)) ||
-        (NULL ==
-            CU_add_test(cuda_matrix_suite,
-                "matrix_t multiplication gpu multi core: shared memory fewer "
-                "accesses larger matrices",
-                test_matrix_multiplication_gpu_multi_core_shared_memory_fewer_accesses_larger_matrices)) ||
-        (NULL == CU_add_test(cuda_qr_decomposition_suite,
-                     "matrix_t qr decomposition single core",
-                     test_matrix_qr_single_core)) ||
+    if (
+        // (NULL == CU_add_test(
+        //              matrix_suite, "matrix_t Init Test", test_init_matrix)) ||
+        // (NULL == CU_add_test(matrix_suite, "matrix_t Init 2x2 From CSV Test",
+        //              test_init_matrix_2x2_from_csv)) ||
+        // (NULL == CU_add_test(matrix_suite, "matrix_t Init 4x1 From CSV Test",
+        //              test_init_matrix_4x1_from_csv)) ||
+        // (NULL == CU_add_test(matrix_suite,
+        //              "matrix_t Init 2x2 doubled From CSV Test",
+        //              test_init_matrix_2x2_doubled_from_csv)) ||
+        // (NULL == CU_add_test(matrix_suite, "matrix_t dimension equality",
+        //              test_matrix_equal_dimensions)) ||
+        // (NULL == CU_add_test(matrix_suite, "matrix_t dimension inequality",
+        //              test_matrix_not_equal_dimensions)) ||
+        // (NULL == CU_add_test(
+        //              matrix_suite, "matrix_t equality", test_matrix_equal)) ||
+        // (NULL == CU_add_test(matrix_suite, "almost equal not",
+        //              test_almost_equal_not)) ||
+        // (NULL ==
+        //     CU_add_test(matrix_suite, "almost equal", test_almost_equal)) ||
+        // (NULL == CU_add_test(matrix_suite, "matrix_t almost equal",
+        //              test_matrix_almost_equal)) ||
+        // (NULL ==
+        //     CU_add_test(matrix_suite, "matrix_t copy", test_matrix_copy)) ||
+        // (NULL == CU_add_test(matrix_suite, "matrix_t addition",
+        //              test_matrix_addition)) ||
+        // (NULL == CU_add_test(matrix_suite, "matrix_t multiplication cpu",
+        //              test_matrix_multiplication)) ||
+        // (NULL == CU_add_test(matrix_suite, "matrix_t qr decomposition cpu",
+        //              test_matrix_qr_decomposition)) ||
+        // (NULL == CU_add_test(matrix_suite, "matrix_t qr 2 decomposition cpu",
+        //              test_matrix_qr_2_decomposition)) ||
+        // (NULL == CU_add_test(cuda_matrix_suite,
+        //              "matrix_t addition gpu single core",
+        //              test_matrix_addition_gpu_single_core)) ||
+        // (NULL == CU_add_test(matrix_suite, "matrix_t Init Test Bad Values",
+        //              test_init_matrix_0_values)) ||
+        // (NULL == CU_add_test(matrix_suite, "matrix_t Random Fill Test In Range",
+        //              test_matrix_random_fill)) ||
+        // (NULL == CU_add_test(cuda_matrix_suite, "Test cuda matrix utility",
+        //              test_cuda_matrix_utility)) ||
+        // (NULL == CU_add_test(cuda_matrix_suite,
+        //              "matrix_t addition gpu multi core",
+        //              test_matrix_addition_gpu_multi_core)) ||
+        // (NULL == CU_add_test(cuda_matrix_suite,
+        //              "matrix_t addition gpu multi core 2",
+        //              test_matrix_addition_gpu_multi_core2)) ||
+        // (NULL == CU_add_test(cuda_matrix_suite,
+        //              "matrix_t addition gpu multi core 2 larger matrices",
+        //              test_matrix_addition_gpu_multi_core2_larger_matrices)) ||
+        // (NULL == CU_add_test(cuda_matrix_suite, "matrix_t addition gpu blocks",
+        //              test_matrix_addition_gpu_blocks)) ||
+        // (NULL == CU_add_test(cuda_matrix_suite,
+        //              "matrix_t multiplication gpu single core",
+        //              test_matrix_multiplication_gpu_single_core)) ||
+        // (NULL == CU_add_test(cuda_matrix_suite,
+        //              "matrix_t multiplication gpu multi core: unwrapping i",
+        //              test_matrix_multiplication_gpu_multi_core_unwrapping_i)) ||
+        // (NULL ==
+        //     CU_add_test(cuda_matrix_suite,
+        //         "matrix_t multiplication gpu multi core: unwrapping i and j",
+        //         test_matrix_multiplication_gpu_multi_core_unwrapping_i_and_j)) ||
+        // (NULL ==
+        //     CU_add_test(cuda_matrix_suite,
+        //         "matrix_t multiplication gpu multi core: unwrapping i and j "
+        //         "for "
+        //         "larger matrices",
+        //         test_matrix_multiplication_gpu_multi_core_unwrapping_i_and_j_larger_matrices)) ||
+        // (NULL ==
+        //     CU_add_test(cuda_matrix_suite,
+        //         "matrix_t multiplication gpu multi core: shared memory",
+        //         test_matrix_multiplication_gpu_multi_core_shared_memory)) ||
+        // (NULL ==
+        //     CU_add_test(cuda_matrix_suite,
+        //         "matrix_t multiplication gpu multi core: shared memory for "
+        //         "larger matrices",
+        //         test_matrix_multiplication_gpu_multi_core_shared_memory_larger_matrices)) ||
+        // (NULL ==
+        //     CU_add_test(cuda_matrix_suite,
+        //         "matrix_t multiplication gpu multi core: shared memory fewer "
+        //         "accesses",
+        //         test_matrix_multiplication_gpu_multi_core_shared_memory_fewer_accesses)) ||
+        // (NULL ==
+        //     CU_add_test(cuda_matrix_suite,
+        //         "matrix_t multiplication gpu multi core: shared memory fewer "
+        //         "accesses larger matrices",
+        //         test_matrix_multiplication_gpu_multi_core_shared_memory_fewer_accesses_larger_matrices)) ||
+        // (NULL == CU_add_test(cuda_qr_decomposition_suite,
+        //              "matrix_t qr decomposition single core",
+        //              test_matrix_qr_single_core)) ||
         (NULL == CU_add_test(cuda_qr_decomposition_suite,
                      "matrix_t qr decomposition parallel max",
                      test_matrix_qr_parallel_max))) {
